bc5cbe8f131eba159ddc3f50c32c93accfef5272
Destiro <debruconn@ecs.vuw.ac.nz>
2020-11-03 22:58:49 +1300

    Made Initial GUI

--- src/GUI/ApplicationView.java
+++ src/GUI/ApplicationView.java
@@ -1,4 +1,59 @@
 package GUI;
 
+import java.awt.*;
+
+import javax.swing.*;
+
 public class ApplicationView {
+  private JFrame window;
+  private String user = "Branch";
+
+  public ApplicationView(){
+    makeDashboard();
+  }
+
+  public void changePage(int newPage){
+    if(newPage == 0){
+      makeDashboard();
+    }else if(newPage == 1){
+      makeExport();
+    }else if(newPage == 2){
+      makeInput();
+    }else if(newPage == 3){
+      makeSettings();
+    }else if(newPage == 4){
+      makeHelp();
+    }else{
+      window.dispose();
+    }
+  }
+
+  private void makeHelp() {
+  }
+
+  private void makeSettings() {
+  }
+
+  private void makeInput() {
+  }
+
+  private void makeExport() {
+  }
+
+  public void makeDashboard(){
+    //General Frame Construction
+    window = new JFrame("KPI Manager");
+    window.setSize(1280, 720);
+    window.setResizable(false);
+    window.setVisible(true);
+    JLabel userSelected = new JLabel(user);
+    window.add(userSelected);
+
+    //Getting Panels
+
+    //Application 
+    
+    //User interaction
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-  public static void main(String[] args){
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import GUI.ApplicationView;
+
+public class Main {
+  private ApplicationView view;
+
+  public static void main(String[] args){
+    /*SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy");
+    System.out.println(dateFormat.format(new Date()));
+    LoadFile.loadYear("assets/"+dateFormat.format(new Date())+".txt");*/
+
+    Main instance = new Main();
+    instance.setup();
+  }
+
+  private void setup(){
+    view = new ApplicationView();
   }
 }
