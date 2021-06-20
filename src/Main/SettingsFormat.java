a4aaa6a48b6170184ba2c3b34c28288abc4f3745
Destiro <connordebruyn234@gmail.com>
2021-06-20 14:01:40 +1200

    Added getters to settingsFormat

--- src/Main/SettingsFormat.java
+++ src/Main/SettingsFormat.java
@@ -1,2 +1,37 @@
-package Main;public class SettingsFormat {
+package Main;
+
+/**
+ * Creates format for the settings file
+ * Storing first year to be loaded from file and targets to reach.
+ */
+public class SettingsFormat {
+  String start_year;
+  Double efficiency_target;
+  Double productivity_target;
+  Double recovery_target;
+
+  SettingsFormat(String start_year, Double efficiency_target,
+                 Double productivity_target, Double recovery_target){
+    this.start_year = start_year;
+    this.efficiency_target = efficiency_target;
+    this.productivity_target = productivity_target;
+    this.recovery_target = recovery_target;
+  }
+
+  //Getters
+  public String getStart_year() {
+    return start_year;
+  }
+
+  public Double getEfficiency_target() {
+    return efficiency_target;
+  }
+
+  public Double getProductivity_target() {
+    return productivity_target;
+  }
+
+  public Double getRecovery_target() {
+    return recovery_target;
+  }
 }
