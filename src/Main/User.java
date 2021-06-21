d317e7d93dc4e6884cf310611b4a828e0743369b
Destiro <connordebruyn234@gmail.com>
2021-06-21 16:57:27 +1200

    Users can now have entries added

--- src/Main/User.java
+++ src/Main/User.java
@@ -1,6 +1,5 @@
 package Main;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -8,16 +7,16 @@ import java.util.HashMap;
  * Stores all their overall stats.
  */
 public class User {
-  public HashMap<Integer, HashMap<Integer, HashMap<Integer, Double[]>>> yearStats;
-  public ArrayList<Double[]> monthAverages;
-  public boolean monthProxyCheck;
+  public HashMap<Integer, HashMap<String, DataFormat[]>> yearStats;
+  public HashMap<String, DataFormat> monthAverages;
+  public DataFormat yearAverage;
+
   public String name;
 
   User(String name){
     this.name = name;
     yearStats = new HashMap<>();
-    monthAverages = new ArrayList<>();
-    monthProxyCheck = false;
+    monthAverages = new HashMap<>();
   }
 
   /**
@@ -26,25 +25,26 @@ public class User {
    * @param year of entry
    * @param month of entry
    * @param week of entry
-   * @param values of data
+   * @param df data entry
    */
-  public void addEntry(int year, int month, int week, Double[] values){
-    monthProxyCheck = false;
+  public void addEntry(int year, String month, int week, DataFormat df){
 
     //Year or Month Doesnt Exist
     if(!yearStats.containsKey(year) || !yearStats.get(year).containsKey(month)){
-      //Initialize new HashMaps
-      HashMap<Integer, HashMap<Integer, Double[]>> monthStats = new HashMap<>();
-      HashMap<Integer, Double[]> weekStats = new HashMap<>();
 
-      //Create Entries
-      weekStats.put(week, values);
-      monthStats.put(month, weekStats);
+      //Initialize an array and fill with nulls + entry
+      DataFormat[] dfList = new DataFormat[6];
+      for(int i=0; i<6; i++){ dfList[i] = null; }
+      dfList[week] = df;
+
+      //Create and add to hashmaps
+      HashMap<String, DataFormat[]> monthStats = new HashMap<>();
+      monthStats.put(month, dfList);
       yearStats.put(year, monthStats);
 
     //Edit an existing entry or week doesnt exist
     }else{
-      yearStats.get(year).get(month).put(week, values);
+      yearStats.get(year).get(month)[week] = df;
     }
   }
 
@@ -55,27 +55,8 @@ public class User {
    * @param month of entry
    * @return a list of weekly values
    */
-  public ArrayList<Double[]> getMonthValues(int year, int month){
-    ArrayList<Double[]> monthValues = new ArrayList<>();
-
-    for(int week : yearStats.get(year).get(month).keySet()){
-      monthValues.set(week-1, yearStats.get(year).get(month).get(week));
-    }
-    return monthValues;
-  }
-
-  /**
-   * Gets a list of averaged monthly values
-   *
-   * @param year
-   * @return
-   */
-  public ArrayList<Double[]> getYearValues(int year){
-    if(!monthProxyCheck){
-      monthProxyCheck = true;
-      setMonthlyAverages();
-    }
-    return monthAverages;
+  public DataFormat[] getMonthValues(int year, int month){
+    return yearStats.get(year).get(month);
   }
 
   /**
@@ -85,10 +66,12 @@ public class User {
   public void setMonthlyAverages(){
     monthAverages.clear();
 
-    for(int month : yearStats.keySet()) {
-      for (int week : yearStats.get(month).keySet()) {
+    for(int year : yearStats.keySet()) {
+      for(String month : yearStats.get(year).keySet()) {
           //todo add all static variables and work out % at the end.
       }
     }
   }
+
+  //Yearly average -> April to March so in july means april-july as year average
 }
